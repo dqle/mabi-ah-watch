502ae367a85806d4ac9ffd5e3c2b47b1ab160288
Duc Le <dqle123@gmail.com>
2022-08-11 19:04:47 -0700

    feat: add feature to alert every 5 min due to mabibase refresh rate

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import sys 
 import os
 from bs4 import BeautifulSoup
+from datetime import datetime
 from discordwebhook import Discord
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -18,6 +19,12 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920,1080")
 driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 
+### Create empty dictionary to keep track of item alert
+itemAlertDict = {}
+
+### Time to resend alert to discord bot in seconds -- this is based on mabibase 5 min refresh rate
+timeAlert = 300
+
 def restart():
     print("argv was",sys.argv)
     print("sys.executable was", sys.executable)
@@ -29,10 +36,10 @@ def main():
     try:
         while True:
             for item in input.itemList():
-                # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
                 ### Variablize Items
                 name = str(item[0])
-                url = "https://na.mabibase.com/tools/auction-house?server=mabius6&q=ItemId%2Ceq%2C%22"+ item[1] + "%22&sort=ItemPrice%3AAscending&page=0"
+                itemID = item[1]
+                url = "https://na.mabibase.com/tools/auction-house?server=mabius6&q=ItemId%2Ceq%2C%22"+ itemID + "%22&sort=ItemPrice%3AAscending&page=0"
                 priceAlert = int(item[2])
                 
                 driver.get(url)
@@ -50,18 +57,23 @@ def main():
                     print("unit price is: "  +  str(f'{unitPrice:,}') )
                     # print("total price is: " +  str(f'{totalPrice:,}') )
                     print("alert price at: " +  str(f'{priceAlert:,}') )
-                    print("")
 
                     if unitPrice <= priceAlert:
-                        discord.post(content="BUY " + name + " at " + str(f'{unitPrice:,}') + " ( Alert Set at : " +  str(f'{priceAlert:,}') + " )")
+                        if itemID in itemAlertDict.keys():
+                            timeDelta = datetime.now() - itemAlertDict.get(itemID)
+                            print(name + " - " + str(timeAlert - timeDelta.total_seconds()) + "s until next alert")
+                            if timeDelta.total_seconds() > timeAlert:
+                                itemAlertDict.pop(itemID)
+                        else:
+                            itemAlertDict[itemID] = datetime.now()
+                            discord.post(content= name + " at " + str(f'{unitPrice:,}') + " ( Alert Set at : " +  str(f'{priceAlert:,}') + " )")
 
-                # driver.quit()
+                    print("---")
     except Exception as e:
         print(e)
-        sys.exit(1)
-    finally:
         driver.quit()
         restart()
+
             
     
 if __name__ == "__main__":
